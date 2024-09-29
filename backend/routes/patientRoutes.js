8f4878c4f5a103df085a05553b133c2ad2703fe1
Inzamamul haque <shaquibhaque8086@gmail.com>
2024-09-29 16:10:49 +0530

    chaes

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Patient = require('../Models/Patient');
+const Patient = require('../Models/patient');
 
 router.get('/', async (req, res) => {
   try {
